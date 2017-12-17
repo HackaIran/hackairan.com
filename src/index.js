fd84ee6f83bc4732fde49e3be03a01d4e4d27a4c
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-12-17 15:40:01 +0330

    Very first React component has been made

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-
-    render () {
-        return (<h1>App</h1>)
-    }
-
-}
+import App from './components/App'
 
 ReactDOM.render(<App />, document.querySelector('#app'));

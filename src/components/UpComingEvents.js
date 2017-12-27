c7a1dd7d73f4bce3f5f959b7a165b20cd58060e6
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-12-27 21:39:12 +0330

    Show Box has been made

--- src/components/UpComingEvents.js
+++ src/components/UpComingEvents.js
@@ -8,7 +8,15 @@ class UpComingEvents extends React.Component {
         return (
             <div style={styles.container}>
                 <div style={styles.showBox}>
-                    <h2>Part</h2>
+                    <div style={styles.selectedEvent}>
+                        <h2 style={styles.eventTitle}>HackaKaraj 3</h2>
+                        <h3 style={styles.eventLocation}>Startup House / <b>Alborz Karaj</b></h3>
+                        <time style={styles.eventDate}>Dec 13 - 25 Aban</time>
+                        <p style={styles.eventDescription}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem ea, in iure molestias reprehenderit tempora. Architecto asperiores at aut, cumque distinctio dolor fugit labore, nobis placeat similique velit voluptas.</p>
+                        <a style={styles.eventRegister} href="https://evand.com" target="_blank">Register</a>
+                    </div>
+                    <div style={styles.timeline}>
+                    </div>
                 </div>
                 <aside style={styles.aside}>
                     <h2>upcoming events</h2>
@@ -24,11 +32,6 @@ class UpComingEvents extends React.Component {
                             <time style={styles.eventItemDate}>Dec 13</time>
                         </li>
                         <li key="item3" style={styles.eventItem}>
-                            <h3 style={styles.eventItemTitle}>HackaKaraj2</h3>
-                            <h4 style={styles.eventItemLocation}>Startup House / <b>Karaj, Iran</b></h4>
-                            <time style={styles.eventItemDate}>Dec 13</time>
-                        </li>
-                        <li key="item4" style={styles.eventItem}>
                             <h3 style={styles.eventItemTitle}>HackaKaraj2</h3>
                             <h4 style={styles.eventItemLocation}>Startup House / <b>Karaj, Iran</b></h4>
                             <time style={styles.eventItemDate}>Dec 13</time>
@@ -49,10 +52,65 @@ const styles = {
         flexDirection: 'row',
         color: 'white'
     },
+    showBox: {
+        flexGrow: 4,
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    selectedEvent: {
+        flexGrow: 1,
+        padding: 40,
+        paddingLeft: 60,
+        paddingRight: 60,
+        position: 'relative'
+    },
+    eventTitle: {
+        fontSize: 35,
+        color: colors.highlight
+    },
+    eventLocation: {
+        fontWeight: 'normal',
+        fontSize: 16,
+        marginTop: 5
+    },
+    eventDate: {
+        fontSize: 19,
+        color: '#888',
+        fontWeight: 'bold',
+        marginTop: 15,
+        marginBottom: 25,
+        display: 'block'
+    },
+    eventDescription: {
+        fontSize: 16,
+        fontWeight: 100,
+        lineHeight: '25px'
+    },
+    eventRegister: {
+        background: colors.highlight,
+        padding: '18px 45px',
+        fontSize: 18,
+        color: 'black',
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        position: 'absolute',
+        top: 47,
+        right: 60,
+
+        ':hover': {
+            opacity: 0.9,
+        }
+    },
+    timeline: {
+        background: 'linear-gradient(transparent, rgba(255, 255, 255, 0.2)',
+        flexBasis: 180,
+        minHeight: 180,
+    },
     aside: {
         background: colors.dark,
         flexGrow: 1,
         padding: 40,
+        minWidth: 330,
         display: 'flex',
         flexDirection: 'column'
     },
@@ -79,12 +137,15 @@ const styles = {
     },
     eventItemTitle: {
         color: colors.highlight,
-        marginTop: 15
+        marginTop: 15,
+        userSelect: 'none',
     },
     eventItemLocation: {
         fontWeight: 100,
-        marginTop: 15,
-        marginBottom: 15
+        fontSize: 13,
+        marginTop: 10,
+        marginBottom: 15,
+        userSelect: 'none',
     },
     eventItemDate: {
         color: 'white',
@@ -96,11 +157,9 @@ const styles = {
         borderRadius: 20,
         position: 'absolute',
         top: '50%',
-        right: 0,
-        transform: 'translateY(-50%)'
-    },
-    showBox: {
-        flexGrow: 4
+        right: 10,
+        transform: 'translateY(-50%)',
+        userSelect: 'none',
     }
 };
 

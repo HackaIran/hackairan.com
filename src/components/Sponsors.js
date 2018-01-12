8dc7aa80123c3dd4452ef37cac2e776f74ecf111
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2018-01-12 21:26:17 +0330

    Sponsors loads from state.sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -11,18 +11,32 @@ class Sponsors extends React.Component {
             sponsors: [
                 {
                     name: 'Reyhoon',
-                    image: 'reyhoon.jpg',
+                    image: 'reyhoon.png',
                     link: 'https://reyhoon.com/'
-                }
+                },
+                {
+                    name: 'Google',
+                    image: 'google.svg',
+                    link: 'https://google.com/'
+                },
+                {
+                    name: 'Tesla Motors',
+                    image: 'tesla.png',
+                    link: 'https://tesla.com/'
+                },
             ]
         };
     }
 
     get sponsors () {
         const sponsors = [];
-        for (let sponsor of this.state.sponsers) sponsers.push(
-            <div>
-                <img src={sponsor.image} alt={`Hacka Sponsors | ${sponsor.name}`} />
+        for (let sponsor of this.state.sponsors) sponsors.push(
+            <div key={sponsor.name} style={styles.sponsor}>
+                <a href={sponsor.link} target="_blank">
+                    <img style={styles.image}
+                        src={`./assets/images/sponsors/${sponsor.image}`}
+                        alt={`Hacka Sponsors | ${sponsor.name}`} />
+                </a>
             </div>
         );
         return sponsors;
@@ -32,7 +46,7 @@ class Sponsors extends React.Component {
         return (
             <div style={styles.container}>
                 <h3 style={styles.title}>Our Sponsors, <strong style={styles.strong}>Our Friends!</strong></h3>
-                <div>{ this.sponsors }</div>
+                <div style={styles.sponsorsContainer}>{ this.sponsors }</div>
             </div>
         )
     }
@@ -54,6 +68,34 @@ const styles = {
     strong: {
         color: colors.highlight,
     },
+    sponsorsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    sponsor: {
+        padding: 20,
+        transitionDuration: '0.2s',
+        cursor: 'pointer',
+
+        ':hover': {
+            transform: 'scale(1.1)',
+            opacity: 0.8,
+        },
+
+        ':active': {
+            opacity: 0.4,
+        }
+    },
+    image: {
+        width: 100,
+    },
+    sponsorName: {
+        color: 'white',
+        textAlign: 'center'
+    }
 };
 
-export default Radium(Sponsers)
+export default Radium(Sponsors)

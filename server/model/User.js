9f9f7fc880d8b922370c2533a269ead4aa0dba91
pmzi <pouya.mozafar@gmail.com>
2018-06-11 19:36:13 +0430

    User Schema defined

---
+++ server/model/User.js
@@ -0,0 +1,37 @@
+const db = require('./db');
+const md5 = require('md5');
+
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = db.Schema({
+    name: String,
+    lastName: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+UserSchema.virrual('fullName').get(function(){
+    return this.name + ' ' + this.lastName;
+})
+
+UserSchema.virtual('gravatar').get(function () {
+    return 'https://www.gravatar.com/avatar/' + md5(this.email);
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const User = db.model('User', userSchema);
+
+module.exports = User;

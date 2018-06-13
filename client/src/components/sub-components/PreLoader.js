60a2b3d9efd8c5b02a32ee82ef151b6ca45b8672
pmzi <pouya.mozafar@gmail.com>
2018-06-13 16:22:47 +0430

    preloader component added

---
+++ client/src/components/sub-components/PreLoader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Radium from 'radium'
+
+class PreLoader extends React.Component {
+    render() {
+        return (
+            <div class="preloader">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="32" width="32" viewBox="0 0 75 75"><circle cx="37.5" cy="37.5" r="33.5" stroke-width="8"></circle></svg>
+            </div>
+        )
+    }
+}
+
+export default Radium(PreLoader)

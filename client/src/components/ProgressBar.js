1b8e55ba34c2b3ac030cc6a13d932ac4aa861ce7
pmzi <pouya.mozafar@gmail.com>
2018-06-13 16:38:04 +0430

    progress bar component has been designed

---
+++ client/src/components/ProgressBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Radium from 'radium'
+
+class ProgressBar extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            style:{
+                width: '0%'
+            }
+        }
+
+    }
+
+    render() {
+        return (
+            <div style={this.state.style} className="progressBar">
+
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        let progressInterVal = setInterval(()=>{
+            if(parseInt(this.state.style.width) == 95){
+                clearInterval(progressInterVal)
+            }else{
+                this.setState({
+                    style:{
+                        width: parseInt(this.state.style.width) + 5 + '%'
+                    }
+                });
+            }
+        },1000)
+    }
+}
+
+export default Radium(ProgressBar)

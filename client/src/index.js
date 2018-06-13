38ca89285e3e7208802fa90b19f92d80ea2fa430
pmzi <pouya.mozafar@gmail.com>
2018-06-13 16:38:32 +0430

    progress bar now disappears after page is loaded

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,10 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.querySelector('#app'));
+
+window.onload = ()=>{
+    document.querySelector(".progressBar").style.width = '100%';
+    setTimeout(()=>{
+        document.querySelector(".progressBar").style.display = 'none';
+    },1000)
+}

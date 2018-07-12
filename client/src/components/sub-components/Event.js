4f2c3ae609cd2dc224b8c847b6d4356b433e8137
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-12 14:08:06 +0430

    single-event's design has been changed

--- client/src/components/sub-components/Event.js
+++ client/src/components/sub-components/Event.js
@@ -1,23 +1,25 @@
 class Event extends React.Component {
     render() {
+        console.log(this.props.image);
         return (
 
             <div className="single-event">
 
-                <div className="single-event__top-gradient" />
-
                 <div className="single-event__content-wrapper">
-                    <div className="single-event__image-wrapper">
-                        <img src={this.props.image} className="single-event__image" />
-                    </div>
 
                     <div className="single-event__description-wrapper">
                         <h3 className="single-event__description-title">
                             {this.props.title}
+                            <span className="highlight">
+                                {this.props.highlight}
+                            </span>
                         </h3>
                         <p className="single-event__description">
                             {this.props.desc}
                         </p>
+                        <a href="#" target="_blank" className="single-event__more-button">
+                            See Through
+                        </a>
                     </div>
                 </div>
 

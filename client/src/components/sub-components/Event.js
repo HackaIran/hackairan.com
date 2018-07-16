daa84a64e26c8ed126c043c3aa57f9d6418252c8
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-17 01:46:32 +0430

    events and products bugs fixed and improvements

--- client/src/components/sub-components/Event.js
+++ client/src/components/sub-components/Event.js
@@ -1,12 +1,17 @@
 class Event extends React.Component {
     render() {
+
+        const orderClass = this.props.order ? 'odd' : 'even';
+
+        const background = (this.props.order ? 'linear-gradient(to right, transparent, black 55%)' : 'linear-gradient(to left, transparent, black 55%)') + `, url('${this.props.image}')`
+        
         return (
 
-            <div className="single-event">
+            <div className={`single-event ${orderClass}`}>
 
-                <div className="single-event__content-wrapper">
+                <div className="single-event__content-wrapper" style={{background}}>
 
-                    <div className="single-event__description-wrapper">
+                    <div className={`single-event__description-wrapper`}>
                         <h3 className="single-event__description-title">
                             {this.props.title}
                             <span className="highlight">
--- client/src/components/sub-components/Product.js
+++ client/src/components/sub-components/Product.js
@@ -1,12 +1,17 @@
 class Product extends React.Component {
     render() {
+
+        const orderClass = this.props.order ? 'odd' : 'even';
+
+        const background = (this.props.order ? 'linear-gradient(to right, transparent, black 55%)' : 'linear-gradient(to left, transparent, black 55%)') + `, url('${this.props.image}')`
+
         return (
 
-            <div className="single-product">
+            <div className={`single-product ${orderClass}`}>
 
                 <div className="single-product__top-gradient" />
 
-                <div className="single-product__content-wrapper">
+                <div className="single-product__content-wrapper" style={{background}}>
 
                     <div className="single-product__description-wrapper">
                         <h3 className="single-product__description-title">

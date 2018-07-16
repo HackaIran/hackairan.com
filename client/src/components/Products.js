0f89e12127b8a195042d7c851e966e9b21d84901
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-17 02:15:04 +0430

    products reordered

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -10,9 +10,8 @@ class Products extends React.Component {
                     Our Products, our <span className="highlight">pride!</span>
                 </h2>
 
-                <Product order={1} link={`/nerdpitch`} desc={ `NerdPitch is an innovative way of representing knowledge and ideas. As the name indicates, it’s a robust way which helps nerds and geeks to disseminate information throughout the world in an interactive way. We have embedded convenient tools within the platform which facilitates the process of creating presentations.` } title='Nerdpitch' />
-
-                <Product order={0} link={`/hope`} image={'assets/images/hope-cover.jpg'} desc={ `HOPE; Hackademy Open-Source Projects Evaluator is an evaluator platform which checks the VCS repository of projects and give an insight over their health; It looks for certain files and global standard metrics within projects and scores them based on their rate of standardization.` } title='HOPE' />
+                <Product order={1} link={`/hope`} image={'assets/images/hope-cover.jpg'} desc={ `HOPE; Hackademy Open-Source Projects Evaluator is an evaluator platform which checks the VCS repository of projects and give an insight over their health; It looks for certain files and global standard metrics within projects and scores them based on their rate of standardization.` } title='HOPE' />
+                <Product order={0} link={`/nerdpitch`} desc={ `NerdPitch is an innovative way of representing knowledge and ideas. As the name indicates, it’s a robust way which helps nerds and geeks to disseminate information throughout the world in an interactive way. We have embedded convenient tools within the platform which facilitates the process of creating presentations.` } title='Nerdpitch' />
 
             </section>
         )

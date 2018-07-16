3e41d729646a2feafcb4fd2a3fc565c682accfb4
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-17 01:54:29 +0430

    header movie has been changed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ class Header extends React.Component {
     render() {
         return (
             <header className="header">
-                <video className="header__video" src="https://as1.cdn.asset.aparat.com/aparat-video/b906acebfc9fb2bdc122c4fe4d4c67079318264-144p__35098.mp4" autoPlay loop={true} />
+                <video className="header__video" src="http://g1.asset.aparat.com/flv_video_new/3740/88587cc3563d963656ee71b9bfe61a3e11217796-240p.apt" autoPlay loop={true} />
                 <div className="header__gradient" />
                 <div className="header__content-wrapper">
                     <img className="header__logo" src="./assets/images/hacka_white.png" alt="Hacka{Iran}"/>

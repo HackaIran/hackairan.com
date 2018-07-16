3a78cefcf8e0bf08ec7256105b49931106b2f174
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-17 02:14:06 +0430

    header video is no longer cdn

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ class Header extends React.Component {
     render() {
         return (
             <header className="header">
-                <video className="header__video" src="http://g1.asset.aparat.com/flv_video_new/3740/88587cc3563d963656ee71b9bfe61a3e11217796-240p.apt" autoPlay loop={true} />
+                <video className="header__video" src="./assets/videos/header.mp4" autoPlay loop={true} />
                 <div className="header__gradient" />
                 <div className="header__content-wrapper">
                     <img className="header__logo" src="./assets/images/hacka_white.png" alt="Hacka{Iran}"/>

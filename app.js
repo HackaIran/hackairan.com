8ac29c07f5d1f9184481e284e5ddd238481af778
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2018-07-17 17:34:07 +0430

    favicon middleware has been fixed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /dist
-//app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
